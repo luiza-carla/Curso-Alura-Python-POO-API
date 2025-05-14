5292a5973993eb480094fa9f8c9947d4abb20ce4
luiza <luizacarla2004@gmail.com>
2025-05-13 21:45:28 -0300

    Adição de herança e polimorfismo

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+from modelos.cardapio.sobremesa import Sobremesa
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiano')
 restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
 
-restaurante_mexicano.receber_avaliacao('Luiza', 9.5)
-restaurante_mexicano.receber_avaliacao('Lucas', 10)
+bebida_suco = Bebida('Suco de Morango', 5.0, 'grande')
+prato_pao = Prato('Pão', 2.0, 'pão recheado')
+sobremesa_pudim = Sobremesa('Pudim', 10, 'Sobremesa de leite condensado')
+bebida_suco.aplicar_desconto()
+prato_pao.aplicar_desconto()
+sobremesa_pudim.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_pao)
+restaurante_praca.adicionar_no_cardapio(sobremesa_pudim)
 
-restaurante_mexicano.alternar_estado()
+# restaurante_mexicano.receber_avaliacao('Luiza', 9.5)
+# restaurante_mexicano.receber_avaliacao('Lucas', 10)
+# restaurante_mexicano.alternar_estado()
 
 def main():
-    Restaurante.listar_restaurantes()
+    # Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,4 +1,5 @@
 from modelos.avaliacao import Avaliacao
+from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Restaurante:
     restaurantes = []
@@ -7,6 +8,7 @@ class Restaurante:
         self._categoria = categoria.title()
         self._ativo = False
         self._avaliacao = []
+        self._cardapio = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -38,3 +40,21 @@ class Restaurante:
         quantidade_notas = len(self._avaliacao)
         media = round(soma_notas/quantidade_notas, 1)
         return media
+    
+    def adicionar_no_cardapio(self, item):
+        if isinstance(item, ItemCardapio):
+            self._cardapio.append(item)
+    
+    @property
+    def exibir_cardapio(self):
+        print(f'cardapio do restaurante {self._nome}\n')
+        for i, item in enumerate(self._cardapio, start=1):
+            if hasattr(item,'descricao'):
+                mensagem_prato = f'{i}. NOME {item._nome} | PREÇO R${item._preco} | DESCRIÇÃO {item.descricao}'
+                print(mensagem_prato)
+            else:
+                mensagem_bebida = f'{i}. NOME {item._nome} | PREÇO R${item._preco} | TAMANHO {item._tamanho}'
+                print(mensagem_bebida)
+
+
+            
